33cf7d389aa2507f9e74fb6d53b441457eb369a4
Mohammad Malekzadeh <moh.malekzadeh@gmail.com>
2018-07-09 14:59:02 +0100

    EDIT

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const https = require('https');
 const express = require("express");
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 const databox = require('node-databox');
 
 //
@@ -12,9 +12,6 @@ const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT
 //HTTPS certs created by the container mangers for this components HTTPS server.
 credentials = databox.getHttpsCredentials();
 
-//conect to databox store 
-let tsc = databox.NewTimeSeriesClient(DATABOX_ZMQ_ENDPOINT, false);
-
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,30 +21,7 @@ app.get("/status", function(req, res) {
 });
 
 app.get("/ui", function(req, res) {
-  res.send("<h1>Hellooooooo!!! world!!!</h1>");
+  res.send("<h1>Hello!!! world!!!</h1>");
 });
-
-
-let ds1 = databox.NewDataSourceMetadata();
-ds1.Description = 'Databox ds1';
-ds1.ContentType = 'application/json';
-ds1.Vendor = 'Databox Inc.';
-ds1.Unit = 'kg';
-ds1.DataSourceType = 'ds1kg';
-ds1.DataSourceID = 'ds1kg_1';
-ds1.StoreType = 'ts';
-
-
-tsc.RegisterDatasource(ds1)
-.then(()=>{
-  tsc.Write('ds1kg_1',{value:34})
-  .catch((err)=>{
-    console.log("Error writing to store")
-  })
-})
-.catch((err)=>{
-  console.log("Error Registering datasource")
-});
-
 
 https.createServer(credentials, app).listen(8080);
